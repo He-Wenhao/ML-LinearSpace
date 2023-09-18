5dc37e678c6d2e1522d9a3da715525bbbd064e4c
htang113 <58230217+htang113@users.noreply.github.com>
2023-09-18 11:43:18 -0400

    Add files via upload

--- workspace.py
+++ workspace.py
@@ -5,30 +5,41 @@ from pkgs.train import trainer;
 from pkgs.dataframe import load_data;
 from pkgs.model import V_theta;
 
-device = 'cpu';
-data, labels = load_data('Methane', device,ind_list=[1,2,3]);
+device = 'cuda';
+data, labels = load_data('Hydrogen', device,ind_list=[i for i in range(250)]);
 
-model = V_theta(device);
 
 train1 = trainer(device, kn=0, lr=0.001);
 train1.optim = torch.optim.Adam(train1.model.parameters(), lr=0.01);
-coef = {'V':1/3,'E':1000/3,'n':0.01/3};
 
 for i in range(100):
+    loss = train1.pretrain(data, labels, kE=0,steps=10);
+    print(loss);
     
-    loss = train1.pretrain_subspace(data, labels, norbs=10, kE=0,steps=10);
+train1.optim = torch.optim.Adam(train1.model.parameters(), lr=1E-3);
+for i in range(50):    
+    loss = train1.pretrain(data, labels, kE=1,steps=10);
     print(loss);
 
-train1.optim = torch.optim.Adam(train1.model.parameters(), lr=1E-4);
-for i in range(100):
-    
-    loss = train1.pretrain_subspace(data, labels,kE=100,steps=10);
+train1.optim = torch.optim.Adam(train1.model.parameters(), lr=1E-3);
+for i in range(50):    
+    loss = train1.pretrain(data, labels, kE=1,steps=10);
     print(loss);
 
-    torch.save(train1.model.state_dict(),'model.pt');
-    
-train1.optim = torch.optim.Adam(train1.model.parameters(), lr=1E-5);
-for i in range(100):
-    
-    loss = train1.finetune(data, labels,coef,steps=10);
-    print(loss);
+# =============================================================================
+# train1.optim = torch.optim.Adam(train1.model.parameters(), lr=1E-4);
+# 
+# for i in range(100):
+#     loss = train1.pretrain_subspace(data, labels,norbs = 10, kE=1,steps=10);
+#     print(loss);
+# 
+#     torch.save(train1.model.state_dict(),'model.pt');
+# =============================================================================
+# =============================================================================
+# coef = {'V':1/3, 'E':1000/3, 'n':0.01/3};
+# train1.optim = torch.optim.Adam(train1.model.parameters(), lr=1E-5);
+# for i in range(100):
+#     
+#     loss = train1.finetune(data, labels,coef,steps=10);
+#     print(loss);
+# =============================================================================
