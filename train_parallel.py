aa9d6e61a1f53ab86e3c01e4c6cf5f3948f0efdd
htang113 <58230217+htang113@users.noreply.github.com>
2023-12-18 22:57:10 -0500

    Add files via upload

--- train_parallel.py
+++ train_parallel.py
@@ -12,44 +12,63 @@ import os;
 
 def example(rank, world_size):
     # create default process group
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
     # create local model
     
+    OPS = {'V':0.1,'E':1,
+           'x':0.1, 'y':0.1, 'z':0.1,
+           'xx':0.05, 'yy':0.05, 'zz':0.05,
+           'xy':0.1, 'yz':0.1, 'xz':0.1}
+
     batch_size = 500;
-    steps_per_epoch = 10;
+    steps_per_epoch = 1;
     N_epoch = 1000;
-    k_E = 0.9;
-    lr_init = 5E-3;
-    lr_final = 1E-5;
+    lr_init = 1E-2;
+    lr_final = 1E-3;
     lr_decay_steps = 50;
     
     molecule_list = [['methane','cyclopropane'],['ethane','propyne'],
                      ['propane','acetylene'],['ethylene','propylene']];
 
-    data, labels = load_data(molecule_list[rank], rank, ind_list=[]);
+    molecule_list = [['methane'],['acetylene']];
+
+    operators_except_EV = [key for key in list(OPS.keys()) if key!='E' and key!='V'];
+    data, labels, obs_mats = load_data(molecule_list[rank], rank, 
+                                       ind_list=range(batch_size), 
+                                       op_names=operators_except_EV);
 
     train1 = trainer_ddp(rank, data, labels, lr=lr_init,
-                         filename=str(rank)+'_model.pt');
+                         filename=str(rank)+'_model.pt',
+                         op_matrices=obs_mats);
     
     scheduler = StepLR(train1.optim, step_size=lr_decay_steps,
                        gamma=(lr_final/lr_init)**(lr_decay_steps/N_epoch));
     
     # define loss function and optimizer
     with open(str(rank)+'.txt','w') as file:
-        file.write('epoch\t loss_V\t loss_E\n');
+        file.write('epoch\t');
+        for i in range(len(OPS)):
+            file.write(' loss_'+str(list(OPS.keys())[i])+'\t');
+        file.write('\n');
         
     # forward pass
     for i in range(N_epoch):
         
-        loss = train1.train(steps=steps_per_epoch, kE = k_E,
-                            batch_size = batch_size);
+        loss = train1.train(steps=steps_per_epoch,
+                            batch_size = batch_size,
+                            op_names=OPS);
         
         scheduler.step();
         with open(str(rank)+'.txt','a') as file:
-            file.write(str(i)+'\t'+str(loss[0])+'\t'+str(loss[1])+'\n');
+            file.write(str(i)+'\t')
+            for i in range(len(loss)):
+                file.write(str(loss[i])+'\t');
+            file.write('\n');
         
 def main():
-    world_size = 4
+    world_size = 2;
     mp.spawn(example,
         args=(world_size,),
         nprocs=world_size,
@@ -59,8 +78,6 @@ if __name__=="__main__":
     # Environment variables which need to be
     # set when using c10d's default "env"
     # initialization mode.
-    os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "29500"
     main()
     
 
--- train_serial.py
+++ train_serial.py
@@ -1,3 +1,4 @@
+import time
 import torch;
 import json;
 import numpy as np;
@@ -6,31 +7,67 @@ from pkgs.dataframe import load_data;
 from pkgs.model import V_theta;
 from torch.optim.lr_scheduler import StepLR;
 
+OPS = {'V':0.1,'E':1,
+       'x':0.1, 'y':0.1, 'z':0.1,
+       'xx':0.1, 'yy':0.1, 'zz':0.1,
+       'xy':0.1, 'yz':0.1, 'xz':0.1}
+
 device = 'cuda:0';
-molecule_list = ['methane','ethane','ethylene','acetylene',
-                 'propane','propylene','propyne'];
-batch_size = 120;
-steps_per_epoch = 10;
+molecule_list = ['methane','ethane','acetylene','ethylene',
+                 'propane','propylene','cyclopropane','propyne',
+                 'cyclobutane','cyclopentane','butane','neopentane',
+                 '2m2b','benzene','isobutane','isobutylene',
+                 '1butyne','2butyne','13butadiene','12butadiene'];
+batch_size = 240;
+steps_per_epoch = 1;
 N_epoch = 1000;
-k_E = 0.9;
-lr_init = 5E-3;
-lr_final = 1E-5;
-lr_decay_steps = 50;
+lr_init = 1E-2;
+lr_final = 1E-3;
+lr_decay_steps = 200;
 
-data, labels = load_data(molecule_list, device, ind_list=[]);
+start_time = time.time()
+def timing(msg):
+    print(f"{msg} | time from start {time.time() - start_time:.1f} s")
 
-train1 = trainer(device,data, labels, lr=lr_init);
+np.set_printoptions(linewidth=np.inf);
+
+print("=" * 75)
+print("PARAMETERS:")
+print("OPS               ", OPS)
+print("device            ", device)
+print("molecule_list     ", molecule_list)
+print("batch_size        ", batch_size)
+print("steps_per_epoch   ", steps_per_epoch)
+print("N_epoch           ", N_epoch)
+print("lr_init           ", lr_init)
+print("lr_final          ", lr_final)
+print("lr_decay_steps    ", lr_decay_steps)
+print("=" * 75)
+
+operators_except_EV = [key for key in list(OPS.keys()) if key!='E' and key!='V'];
+data, labels, obs_mats = load_data(molecule_list, device, 
+                                   ind_list=[2*i for i in range(batch_size)], 
+                                   op_names=operators_except_EV);
+
+timing("data loaded")
+
+train1 = trainer(device,data, labels, lr=lr_init,
+                    op_matrices=obs_mats);
 schedular = StepLR(train1.optim, step_size=lr_decay_steps,
                    gamma=(lr_final/lr_init)**(lr_decay_steps/N_epoch));
 
 Loss = [];
-
 for i in range(N_epoch):
-    
-    loss = train1.train(steps=steps_per_epoch, kE = k_E, batch_size = batch_size);
+
+    loss = train1.train(steps=steps_per_epoch,
+                        batch_size = batch_size,
+                        op_names=OPS);
     print(loss);
     Loss.append(loss.tolist());
     schedular.step();
+    timing(f"epoch {i} done")
+
+timing("training complete")
 
 with open('loss.json','w') as file:
     json.dump(Loss,file);
