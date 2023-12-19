f066959df03c631309ea3f0cc89d2535ded1e029
htang113 <58230217+htang113@users.noreply.github.com>
2023-12-19 17:39:46 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -5,35 +5,40 @@ Created on Thu Aug 17 16:00:28 2023
 @author: 17000
 """
 
-import torch;
-import json;
-import numpy as np;
-import matplotlib;
 from pkgs.dataframe import load_data;
-from pkgs.model import V_theta;
 from pkgs.deploy import estimator;
-import matplotlib.pyplot as plt;
 from pkgs.sample_minibatch import sampler;
-from pkgs.tomat import to_mat;
 
 
-device = 'cpu';
+device = 'cuda:0';
 
-molecule_list = ['methane','ethane','ethylene','acetylene'];
+molecule_list = ['methane','ethane','ethylene','acetylene',
+                 'propane','propylene','propyne','cyclopropane'];
 
-data, labels = load_data(molecule_list, device, ind_list=range(500));
+OPS = {
+       'x':0.1, 'y':0.1, 'z':0.1,
+       'xx':0.1, 'yy':0.1, 'zz':0.1,
+       'xy':0.1, 'yz':0.1, 'xz':0.1};
+
+data, labels, obs_mat = load_data(molecule_list, device, 
+                                  ind_list=range(200),op_names=list(OPS.keys())
+                                  );
+
 sampler1 = sampler(data, labels, device);
 
 est = estimator(device);
-est.load('model_4molecules.pt')
+est.load('model.pt');
 
-batch_size = 500;
+batch_size = 200;
+
 for i in range(len(molecule_list)):
+    E_nn = labels[i]['E_nn']
+    minibatch, labels1 = sampler1.sample(batch_size=batch_size, i_molecule=i,
+                                        op_names=list(OPS.keys()));
     
-    minibatch, labels = sampler1.sample(batch_size=batch_size, i_molecule=i);
-    
-    Ehat, E = est.solve(minibatch,labels,
-                        save_filename=molecule_list[i]);        
+    Ehat, E = est.solve(minibatch, labels1, obs_mat[i], E_nn,
+                        save_filename=molecule_list[i],
+                        op_names = list(OPS.keys()));
 
-est.plot(molecule_list)
+est.plot(molecule_list, nrows=2)
 
