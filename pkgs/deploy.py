526ce50022378a22a5fea137a8d84d8e9f24b9ee
htang113 <58230217+htang113@users.noreply.github.com>
2023-12-19 17:40:15 -0500

    Add files via upload

--- pkgs/deploy.py
+++ pkgs/deploy.py
@@ -31,6 +31,7 @@ class estimator():
         self.loss = torch.nn.MSELoss();
         self.transformer = to_mat(device);
         self.output_folder = output_folder;
+
         if(not os.path.exists(output_folder)):
             os.mkdir(output_folder);
 
@@ -50,7 +51,7 @@ class estimator():
     def set_op_matrices(self, op_matrices):
         self.op_matrices = op_matrices
 
-    def solve(self, minibatch, labels, op_names=None, save_filename='data') -> float:
+    def solve(self, minibatch, labels, obs_mat, E_nn, op_names=None, save_filename='data') -> float:
 
         h = labels['h'];
 
@@ -67,30 +68,31 @@ class estimator():
 
         LA, Phi = torch.linalg.eigh(H.detach());
         Ehat = 2*torch.sum(LA[:, :ne], axis=1);
-
+        
+        obj = {'E':(E+E_nn).tolist(), 'Ehat':(Ehat+E_nn).tolist()};
         if op_names is not None:
-            ops = {}
             co = Phi[:, :, :ne]
             for op_name in op_names:
                 O = labels[op_name]
-                O_mat = self.op_matrices[op_name]
-                Ohat = 2 * torch.einsum('jui,uv,jvi->j', [co, O_mat, co])
-                ops[op_name] = Ohat
-            with open(self.output_folder+'/'+save_filename+'_obs.json','w') as file:
-                json.dump(ops,file)
+                O_mat = obs_mat[op_name]
+                Ohat = 2 * torch.einsum('jui,juv,jvi->j', [co, O_mat, co])
+                obj[op_name] = {'Ohat': Ohat.tolist(),
+                                'O': O.tolist()};
 
         with open(self.output_folder+'/'+save_filename+'.json','w') as file:
-            json.dump({'Ee':E.tolist(), 'Ehat':Ehat.tolist()},file)
+            json.dump(obj,file)
 
 
-        return Ehat, E, ops;
+        return Ehat, E;
 
-    def plot(self, molecule_list):
+    def plot(self, molecule_list, nrows=1):
 
         font = {'size' : 18}
-
+        
+        ncols = int((len(molecule_list)-1)//nrows)+1; 
+        
         matplotlib.rc('font', **font)
-        plt.figure(figsize=(6*len(molecule_list),5.5));
+        plt.figure(figsize=(6*ncols,5.5*nrows));
         res = [];
         for i in range(len(molecule_list)):
 
@@ -98,12 +100,15 @@ class estimator():
 
                 data = json.load(file);
 
-            E = data['Ee'];
+            E = data['E'];
             Ehat = data['Ehat'];
 
-            plt.subplot(1,len(molecule_list),i+1);
-
+            plt.subplot(nrows,ncols,i+1);
+            
+            plt.plot([np.min(E)*27.211,np.max(E)*27.211], [np.min(E)*27.211,np.max(E)*27.211], 
+                     linestyle='dashed', linewidth = 3,c='red');
             plt.scatter(np.array(E)*27.211,np.array(Ehat)*27.211);
+            
             plt.title(molecule_list[i]);
             plt.xlabel('E$_{CCSDT}$  (eV)');
             plt.ylabel('E$_{NN}$  (eV)');
@@ -113,7 +118,48 @@ class estimator():
 
         print('Standard deviation error:');
         print(str(np.sqrt(np.mean(res))/1.594*10**3)+' kcal/mol');
+        plt.savefig(self.output_folder+'/E.png');
+        plt.close();
+        
+        for ops in ['x','y','z','xx','yy','zz','xy','xz','yz']:
+            font = {'size' : 18}
+            
+            ncols = int((len(molecule_list)-1)//nrows)+1; 
+            
+            matplotlib.rc('font', **font)
+            plt.figure(figsize=(6*ncols,5.5*nrows));
+            res = [];
+            
+            if(len(ops)==1):
+                unit = 'Debye'
+            else:
+                unit = 'e*a$_0^2$';
+                
+            for i in range(len(molecule_list)):
 
+                with open(self.output_folder+'/'+molecule_list[i]+'.json','r') as file:
+
+                    data = json.load(file);
+
+                O = data[ops]['O'];
+                Ohat = data[ops]['Ohat'];
+
+                plt.subplot(nrows,ncols,i+1);
+
+                plt.plot([np.min(O),np.max(O)], [np.min(O),np.max(O)], 
+                         linestyle='dashed', linewidth = 3,c='red');
+                plt.scatter(np.array(O),np.array(Ohat));
+                plt.title(molecule_list[i]);
+                plt.xlabel(ops+'$_{CCSDT}$  '+unit);
+                plt.ylabel(ops+'$_{NN}$  '+unit);
+                res.append(np.mean((np.array(O)-np.array(Ohat))**2));
+
+            plt.tight_layout();
+
+            print(ops+': Standard deviation error:');
+            print(str(np.sqrt(np.mean(res)))+' '+unit);
+            plt.savefig(self.output_folder+'/'+ops+'.png');
+            plt.close();
 
 
 
