208f08f696ec2035d946359a75f4415b8341113a
htang113 <58230217+htang113@users.noreply.github.com>
2023-12-21 11:30:06 -0500

    Add files via upload

--- script/generate_ccsdt.py
+++ script/generate_ccsdt.py
@@ -3,86 +3,99 @@ from ase.io.trajectory import Trajectory
 from ase import io;
 import os;
 
-def write(filename,atoms):
-    dic = {1:'H', 6:'C'};
-    with open(filename,'w') as file:
-        file.write('! CCSD(T) EOM-CCSD cc-pVTZ cc-pVTZ/C\n');
-        file.write('! LargePrint PrintBasis KeepDens\n');
-        file.write('%maxcore 3000\n');
-#        file.write('%pal\n');
-#        file.write('nprocs 32\n');
-#        file.write('end\n');
-        file.write('%MDCI\n');
-        file.write('  nroots 3\n');
-        file.write('  triples 1\n');
-#        file.write(' MAXITER 500 \n');
-#        file.write('  MaxDIIS 100\n');
-#        file.write('  Lshift 0.01\n');
-#        file.write('  density orbopt\n');
-        file.write('END\n');
+NPAR = 6;
 
-        file.write('%ELPROP\n');
-        file.write('  dipole true\n');
-        file.write('  quadrupole true\n');
-        file.write('END\n');
+def write(filename, atoms, type_calc):
+    
+    if(type_calc == 'pvtz'):
+        dic = {1:'H', 6:'C'};
+        with open(filename,'w') as file:
+            file.write('! CCSD(T) cc-pVTZ cc-pVTZ/C\n');
+            file.write('%maxcore 3000\n');
+            file.write('%pal\n');
+            file.write('nprocs '+str(NPAR)+'\n');
+            file.write('end\n');
+            file.write('! LargePrint KeepDens\n');
+            file.write('%MDCI\n');
+            file.write('  triples 1\n');
+            file.write('END\n');
+    
+            file.write('%ELPROP\n');
+            file.write('  dipole true\n');
+            file.write('  quadrupole true\n');
+            file.write('END\n');
+    
+            file.write('% OUTPUT\n');
+            file.write('  Print[ P_Density ] 1        # converged density\n');
+            file.write('  Print[ P_KinEn ] 1          # kinetic energy matrix\n');
+            file.write('END\n');
+    
+            file.write('* xyz 0 1 \n');
+            pos = atoms.get_positions();
+            atm = atoms.get_atomic_numbers();
+            n = len(pos);
+            for i in range(n):
+                file.write(dic[atm[i]]+'\t');
+                for j in range(3):
+                    file.write(str(pos[i][j])+'\t');
+                file.write('\n');
+            file.write('*');
+    elif(type_calc =='pvdz'):
 
-#        file.write('%SCF\n');
-#        file.write('  MAXITER 500\n');
-##        file.write('  TolE 1e-6\n');
-#        file.write('END\n');
+        dic = {1:'H', 6:'C'};
+        with open(filename,'w') as file:
+            file.write('! HF cc-pVDZ\n');
+            file.write('! LargePrint PrintBasis KeepDens\n');
+            file.write('%maxcore 3000\n');
 
-        file.write('% OUTPUT\n');
-        file.write('  Print[ P_Density ] 1        # converged density\n');
-        file.write('  Print[ P_OneElec ] 1        # one electron matrix\n');
-        file.write('  Print[ P_Overlap ] 1        # overlap matrix\n');
-        file.write('  Print[ P_KinEn ] 1          # kinetic energy matrix\n');
-        file.write('  Print[ P_Iter_F ] 1         # Fock matrix, for every iteration\n');
-        file.write('  Print[ P_Fockian ] 1        # Fockian (not sure what this is)\n');
-        file.write('END\n');
+            file.write('% OUTPUT\n');
+            file.write('  Print[ P_Overlap ] 1        # overlap matrix\n');
+            file.write('  Print[ P_Iter_F ] 1         # Fock matrix, for every iteration\n');
+            file.write('END\n');
+    
+            file.write('* xyz 0 1 \n');
+            pos = atoms.get_positions();
+            atm = atoms.get_atomic_numbers();
+            n = len(pos);
+            for i in range(n):
+                file.write(dic[atm[i]]+'\t');
+                for j in range(3):
+                    file.write(str(pos[i][j])+'\t');
+                file.write('\n');
+            file.write('*');
 
-        file.write('* xyz 0 1 \n');
-        pos = atoms.get_positions();
-        atm = atoms.get_atomic_numbers();
-        n = len(pos);
-        for i in range(n):
-            file.write(dic[atm[i]]+'\t');
-            for j in range(3):
-                file.write(str(pos[i][j])+'\t');
-            file.write('\n');
-        file.write('*');
 
-l = os.listdir('./');
 route = os.getcwd()+'/';
-for fd in l:
-#    if(fd[-3:]!='.py' and fd!='test'):
-    if os.path.isdir(fd):
-        traj = Trajectory(route+fd+'/data.traj');
-        io.write(route+fd+'/XDATCAR', traj, format='vasp-xdatcar');
+traj = Trajectory(route+'/data.traj');
+os.mkdir('pvdz');
+os.mkdir('pvtz');
 
-        steps = 1;
-        i_index = 0;
-        for atoms in traj:
-            if(str(i_index) not in os.listdir(fd)):
-                os.mkdir(route+fd+'/'+str(i_index));
-            if(i_index%steps == 0):
-                write(route+fd+'/'+str(i_index)+'/run.inp',atoms);
+for folder in ['pvdz','pvtz']:
 
-            i_index += 1;
-        with open(route+fd+'/submit.sh','w') as file:
-            file.write('#!/bin/bash\n');
-            file.write('#SBATCH -o log-%j\n');
-            file.write('#SBATCH -N 1\n');
-            file.write('#SBATCH -n 48\n');
-            file.write('#SBATCH --exclusive\n');
-            file.write('#SBATCH -p xeon-p8\n\n');
-            file.write('module load mpi/openmpi-4.1.1\n\n');
-            file.write('for i in {0..499}\n');
-            file.write('do\n    cd $i\n /home/gridsan/hxu1/apps/orca/orca  run.inp > log\n');
-            file.write('    cd ../\n');
-            file.write('done');
-        print(fd);
+    i_index = 0;
+    for atoms in traj:
+        os.mkdir(route+folder+'/'+str(i_index));
 
-        os.chdir(fd)
-        os.system("sbatch submit.sh")
-        os.chdir("../")
+        write(route+folder+'/'+str(i_index)+'/run.inp',atoms,folder);
+    
+        i_index += 1;
+        
+    with open(route+folder+'/submit.sh','w') as file:
+        file.write('#!/bin/bash\n');
+        file.write('#SBATCH -o log-%j\n');
+        file.write('#SBATCH -N 1\n');
+        file.write('#SBATCH -n 48\n');
+        file.write('#SBATCH -p xeon-p8\n');
+        file.write('#SCATCH --exclusive\n\n');
+        file.write('module load mpi/openmpi-4.1.5\n\n');
+        
+        file.write('for i in {0..499}\n');
+        file.write('do\n    cd $i\n    /home/gridsan/htang/orca/orca  run.inp > log\n');
+        file.write('    wait\n')
+        file.write('    cd ../\n');
+        file.write('done');
+    
+    os.chdir(folder)
+#    os.system("sbatch submit.sh")
+    os.chdir(route)
 
