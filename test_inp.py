6fa9e6bd69989e4a3d5f63aacc487551310e2f5a
htang113 <58230217+htang113@users.noreply.github.com>
2024-08-03 23:28:50 -0400

    Update test_inp.py

--- test_inp.py
+++ test_inp.py
@@ -10,7 +10,7 @@ params['OPS'] = {'E':1,
     'bond_order':0.02, 'alpha':3E-5};
 
 params['device'] = 'cuda:0';
-params['batch_size'] = 50;
+params['batch_size'] = 40;
 params['scaling'] = {'V':1, 'T': 0.01};
 params['element_list'] = ['H','C','N','O','F'];
 params['path'] = '/pscratch/sd/t/th1543/v2.0/';
