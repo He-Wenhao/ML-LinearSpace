9e55054e9f24a274a606e47247e176429316eb0e
htang113 <58230217+htang113@users.noreply.github.com>
2024-08-14 12:16:20 -0400

    Delete deploy directory

--- deploy/__init__.py
+++
@@ -1,6 +0,0 @@
-from deploy.predictor import predict_fns;
-from deploy.deploy_cls import estimator_test;
-from deploy.apply import estimator;
-from deploy.apply import load_data;
-from deploy.apply import sampler;
-from deploy.test_func import test_func;
