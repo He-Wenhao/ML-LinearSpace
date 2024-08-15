b7549a10c357a12c407d8dcb6e19f4811c00811d
htang113 <58230217+htang113@users.noreply.github.com>
2024-08-14 23:51:25 -0400

    Update read.py

--- demo/infer_orca/read.py
+++ demo/infer_orca/read.py
@@ -11,10 +11,10 @@ matrix_dic = reader.read_matrix('');
 
 output = {};
 for key in HF_dic:
-    output[key] = [HF_dic[key]];
+    output[key] = HF_dic[key];
 for key in matrix_dic:
-    output[key] = [matrix_dic[key]];
-output['name'] = [system];
+    output[key] = matrix_dic[key];
+output['name'] = system;
 
 with open(path + system + '.json','w') as file:
     json.dump(output, file);
